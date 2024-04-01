19b3621459ee6eeda4ac0395f6791d0638e5ec1f
JayJHFE <dove92lee@gmail.com>
2024-04-01 20:53:41 +0900

    스케줄러 시작

--- PortFolio-vite/src/App.tsx
+++ PortFolio-vite/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import * as S from "./css/mainStyle";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
+      <S.container>아래의 메뉴 중 선택해 주세요</S.container>
       <div>
+        <div>리액트 + 바이트 기본페이지</div>
+        <div>스케줄러</div>
+      </div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +33,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
