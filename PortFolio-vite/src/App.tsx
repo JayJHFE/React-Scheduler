11b3110dd5baaa23d9b53439bccb0461c433b5ba
JayJHFE <dove92lee@gmail.com>
2024-04-05 21:18:57 +0900

    일부 구조 수정

--- PortFolio-vite/src/App.tsx
+++ PortFolio-vite/src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import * as S from "./css/mainStyle.ts";
 import Main from "./page/main/index.tsx";
 import Scheduler from "./page/scheduler/index.tsx";
 import { Link, Route, Routes } from "react-router-dom";
@@ -7,19 +6,14 @@ import { Link, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <S.container>아래의 메뉴 중 선택해 주세요</S.container>
-      <S.innerContainer>
+      <div>
         <Routes>
+          <Route path="/" element={<Main />} />
           <Route path="/main" element={<Main />} />
           <Route path="/scheduler" element={<Scheduler />} />
         </Routes>
-        <S.innerLeftContainer>
-          <Link to="/main">리액트 + 바이트 기본페이지</Link>
-        </S.innerLeftContainer>
-        <S.innerRightContainer>
-          <Link to="/scheduler">스케줄러 페이지 </Link>
-        </S.innerRightContainer>
-      </S.innerContainer>
+        <Link to="/main">저를 클릭해 주세요</Link>
+      </div>
     </>
   );
 }
