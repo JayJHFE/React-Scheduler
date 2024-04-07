4043a519efa00d70e59a29dbe11654d17042c695
JayJHFE <dove92lee@gmail.com>
2024-04-07 21:41:50 +0900

    스케줄러 구조설정

--- PortFolio-vite/src/page/scheduler/index.tsx
+++ PortFolio-vite/src/page/scheduler/index.tsx
@@ -1,7 +1,21 @@
 export default function Scheduler() {
   return (
     <>
-      <div>스케줄러 페이지 테스트 입니다</div>
+      <div>
+        메인컨테이너
+        <div>상단 컨테이너</div>
+        <div>
+          하단 컨테이너
+          <div>
+            좌측 컨테이너
+            <div>스케줄러 표기</div>
+          </div>
+          <div>
+            우측 컨테이너
+            <div>일정용 리스트 표기</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
