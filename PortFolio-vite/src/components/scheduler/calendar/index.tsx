bfbc2643270053354e03e535062f7f8a7d6fb013
JayJHFE <dove92lee@gmail.com>
2024-04-15 20:13:29 +0900

    스케줄러 수정중

--- PortFolio-vite/src/components/scheduler/calendar/index.tsx
+++ PortFolio-vite/src/components/scheduler/calendar/index.tsx
@@ -1,22 +1,37 @@
 import dayjs from "dayjs";
 import DateAndDay from "./dateAndDay";
+dayjs.locale("ko");
 
 function Calendar() {
-  // 오늘 날짜
-  const currentDate = dayjs();
-  // 이번달에 해당하는 날짜 수 추출
-  const daysInMonth = currentDate.daysInMonth();
+  // // 오늘 날짜
+  // const currentDate = dayjs();
+  // // 이번달에 해당하는 날짜 수 추출
+  // const daysInMonth = currentDate.daysInMonth();
 
-  // 현재 달의 모든 날짜와 요일에 대한 배열 생성
+  // // 현재 달의 모든 날짜와 요일에 대한 배열 생성
 
-  // 현재 달의 모든 날짜에 대한 배열 생성
-  const daysArray = Array.from(
-    { length: daysInMonth },
-    (_, index) => index + 1
-  );
+  // // 현재 달의 모든 날짜에 대한 배열 생성
+  // const daysArray = Array.from(
+  //   { length: daysInMonth },
+  //   (_, index) => index + 1
+  // );
 
-  //각 날짜에 대한 요일 생성
-  const dayOfWeek = currentDate.date(1).day();
+  const getAllDaysAndWeekdaysInMonth = (year: number, month: number) => {
+    const startDate = dayjs(`${year}-${month}-01`); // 월의 첫 날
+    const daysInMonth = startDate.daysInMonth(); // 해당 월의 일수
+    let daysWithWeekdays = [];
+
+    for (let day = 1; day <= daysInMonth; day++) {
+      const currentDay = startDate.date(day);
+      daysWithWeekdays.push({
+        date: currentDay.format("YYYY-MM-DD"), // 날짜
+        weekdayNumber: currentDay.day(), // 요일 번호
+        weekdayName: currentDay.format("dddd"), // 요일 이름
+      });
+    }
+
+    return daysWithWeekdays;
+  };
 
   return (
     <div>
