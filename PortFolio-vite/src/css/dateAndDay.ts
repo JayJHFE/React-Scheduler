fffe3ea50a2516d05b44c386fbcd6efa65ae3866
JayJHFE <dove92lee@gmail.com>
2024-04-17 21:26:38 +0900

    스케줄러 상단달력 스크롤 활성화

--- PortFolio-vite/src/css/dateAndDay.ts
+++ PortFolio-vite/src/css/dateAndDay.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const dateDiv = styled.div`
-  width: 50px;
-  height: 80px;
+  width: 100px;
+  height: 100px;
   color: #000000;
   background-color: #ffffff;
+  flex: 0 0 auto;
+  width: 100px;
+  padding: 10px;
 `;
--- PortFolio-vite/src/css/scheduler.ts
+++ PortFolio-vite/src/css/scheduler.ts
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const rowScheduler = styled.div`
   display: flex;
   flex-direction: row;
+  overflow-x: auto;
+  width: 100%;
+  white-space: nowrap;
 `;
