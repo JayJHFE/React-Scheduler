8e98899bd5f696ba28cf708d1da5d3ab5e6655fb
JayJHFE <dove92lee@gmail.com>
2024-04-20 12:51:19 +0900

    스케줄러 달력 div 높이변경

--- PortFolio-vite/src/css/dateAndDay.ts
+++ PortFolio-vite/src/css/dateAndDay.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const dateDiv = styled.div`
   width: 100px;
-  height: 100px;
+  height: 120px;
   color: #000000;
   background-color: #ffffff;
   flex: 0 0 auto;
