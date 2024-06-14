04edf424e7ad11f5f46cb1c3400e623cf676d00c
JayJHFE <dove92lee@gmail.com>
2024-06-14 22:07:15 +0900

    스케줄러 UI수정

--- PortFolio-vite/src/page/scheduler/index.tsx
+++ PortFolio-vite/src/page/scheduler/index.tsx
@@ -9,7 +9,10 @@ export default function Scheduler() {
   return (
     <>
       <S.Container>
-        <Calendar />
+        <div>
+          <div>월 달력</div>
+          <Calendar />
+        </div>
         <S.innerContainer>
           <div style={{ color: "blue", display: "flex", flexDirection: "row" }}>
             <div
