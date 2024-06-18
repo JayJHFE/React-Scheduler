9f784105dd095791ba3c461160f764a195eec978
JayJHFE <dove92lee@gmail.com>
2024-06-18 22:14:18 +0900

    스케줄러 UI수정

--- PortFolio-vite/src/page/scheduler/index.tsx
+++ PortFolio-vite/src/page/scheduler/index.tsx
@@ -4,6 +4,9 @@ import jsonData from "../../../public/test.json";
 
 export default function Scheduler() {
   const data = jsonData.vehiclces;
+  const hoursArray = Array.from({ length: 24 }, (_, i) =>
+    i === 0 ? "차량" : i
+  );
   console.log(data);
   console.log(typeof data);
   return (
@@ -20,72 +23,64 @@ export default function Scheduler() {
             <Calendar />
           </div>
           <div style={{ color: "blue", display: "flex", flexDirection: "row" }}>
-            <div
-              style={{
-                color: "blue",
-                display: "flex",
-                flexDirection: "row",
-                width: "400px",
-              }}
-            >
-              차량
-            </div>
-            {Array.from({ length: 24 }, (_, i) => (
+            {hoursArray.map((hour, index) => (
               <div
                 style={{
                   backgroundColor: "red",
-                  width: "400px",
+                  width: index === 0 ? "600px" : "400px",
                   border: "1px solid black",
                 }}
-                key={i}
+                key={index}
               >
-                {i}
+                {hour}
               </div>
             ))}
           </div>
-          {data &&
-            data[0].map((vehicle) => (
-              <div style={{ display: "flex", flexDirection: "row" }}>
-                <div></div>
-                <div
-                  style={{ backgroundColor: "blue", width: "400px" }}
-                  key={vehicle.id}
-                >
-                  {vehicle.name}
-                </div>
-                {Array.from({ length: 24 }, (_, i) => (
+          <div>
+            {data &&
+              data[0].map((vehicle) => (
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <div></div>
                   <div
-                    style={{
-                      backgroundColor: "red",
-                      width: "400px",
-                      border: "1px solid black",
-                    }}
-                    key={i}
+                    style={{ backgroundColor: "blue", width: "600px" }}
+                    key={vehicle.id}
                   >
+                    {vehicle.name}
+                  </div>
+                  {Array.from({ length: 24 }, (_, i) => (
                     <div
                       style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        width: "60px",
+                        backgroundColor: "red",
+                        width: "600px",
+                        border: "1px solid black",
                       }}
+                      key={i}
                     >
-                      {Array.from({ length: 60 }, (_, i) => (
-                        <div
-                          style={{
-                            backgroundColor: "red",
-                            width: "1px",
-                            border: "1px solid black",
-                          }}
-                          key={i}
-                        >
-                          {i}
-                        </div>
-                      ))}
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "row",
+                          width: "60px",
+                        }}
+                      >
+                        {Array.from({ length: 60 }, (_, i) => (
+                          <div
+                            style={{
+                              backgroundColor: "red",
+                              width: "1px",
+                              border: "1px solid black",
+                            }}
+                            key={i}
+                          >
+                            {i}
+                          </div>
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-            ))}
+                  ))}
+                </div>
+              ))}
+          </div>
         </S.innerContainer>
       </S.Container>
     </>
