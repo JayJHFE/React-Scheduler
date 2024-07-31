3e851380d6c961146806205d780c45d01657d63b
JayJHFE <dove92lee@gmail.com>
2024-07-31 21:18:21 +0900

    스케줄러안쪽 div 생성

--- PortFolio-vite/src/components/scheduler/table/index.tsx
+++ PortFolio-vite/src/components/scheduler/table/index.tsx
@@ -2,31 +2,35 @@ function SchedulerTable() {
   //25개의 시간을 만들어서 차량을 넣어준다.
 
   const hoursArray = Array.from({ length: 25 }, (_, i) => i);
+  const data = 5;
   return (
     <>
-      <div
-        style={{
-          width: "4500px",
-          color: "blue",
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        {/* hourArray 를 data의 길이만큼 생성*/}
-        {hoursArray.map((hour, index) => (
-          <div
-            style={{
-              backgroundColor: "red",
-              width: "180px",
-              height: "30px",
-              border: "1px solid black",
-            }}
-            key={index}
-          >
-            {hour}
-          </div>
-        ))}
-      </div>
+      {/* div 를 data의 길이만큼 생성 */}
+      {Array.from({ length: data }, (_, i) => (
+        <div
+          key={i}
+          style={{
+            width: "4500px",
+            color: "blue",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          {hoursArray.map((hour, index) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: "180px",
+                height: "30px",
+                border: "1px solid black",
+              }}
+              key={index}
+            >
+              {hour}
+            </div>
+          ))}
+        </div>
+      ))}
     </>
   );
 }
