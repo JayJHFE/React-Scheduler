7a2c87003d56dcc83fb107ac0803fc67f6283cf6
JayJHFE <dove92lee@gmail.com>
2024-08-20 21:03:00 +0900

    스케줄러 수정

--- PortFolio-vite/src/components/scheduler/table/index.tsx
+++ PortFolio-vite/src/components/scheduler/table/index.tsx
@@ -5,44 +5,51 @@ function SchedulerTable() {
   const data = 5;
   return (
     <>
-      {/* 세로로 위에 덮는 div 를 data의 길이만큼 생성 */}
-      {Array.from({ length: data }, (_, i) => (
-        <div
-          key={i}
-          style={{
-            width: "4500px",
-            color: "blue",
-            display: "flex",
-            flexDirection: "row",
-          }}
-        ></div>
-      ))}
-      {/* div 를 data의 길이만큼 생성 */}
-      {Array.from({ length: data }, (_, i) => (
-        <div
-          key={i}
-          style={{
-            width: "4500px",
-            color: "blue",
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          {hoursArray.map((hour, index) => (
-            <div
-              style={{
-                backgroundColor: "red",
-                width: "180px",
-                height: "30px",
-                border: "1px solid black",
-              }}
-              key={index}
-            >
-              {hour}
-            </div>
-          ))}
-        </div>
-      ))}
+      <div style={{ position: "relative", top: "150px" }}>
+        {/* 세로로 위에 덮는 div 를 data의 길이만큼 생성 */}
+        {/* div를 absolute로 설정하여 겹치게 만든다. */}
+        {Array.from({ length: data }, (_, i) => (
+          <div
+            key={i}
+            style={{
+              zIndex: 100,
+              width: "180px",
+              backgroundColor: "blue",
+              display: "flex",
+              flexDirection: "column",
+              position: "absolute",
+            }}
+          >
+            1
+          </div>
+        ))}
+        {/* div 를 data의 길이만큼 생성 */}
+        {Array.from({ length: data }, (_, i) => (
+          <div
+            key={i}
+            style={{
+              width: "4500px",
+              color: "blue",
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            {hoursArray.map((hour, index) => (
+              <div
+                style={{
+                  backgroundColor: "red",
+                  width: "180px",
+                  height: "30px",
+                  border: "1px solid black",
+                }}
+                key={index}
+              >
+                {hour}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
