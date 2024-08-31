6c5121a6c60b38299da3c78362f0aeb6acdf156c
JayJHFE <dove92lee@gmail.com>
2024-08-31 23:24:57 +0900

    스케줄러 수정

---
+++ PortFolio-vite/src/components/scheduler/right/rightUpperContainer.tsx
@@ -0,0 +1,9 @@
+function RightUpperContainer() {
+  return (
+    <div className="right-upper-container">
+      <div className="right-upper-container__title">Scheduler</div>
+      <div className="right-upper-container__date">Today</div>
+    </div>
+  );
+}
+export default RightUpperContainer;
