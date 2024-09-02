29c476a3aa9ec2cc67a8f203939333be5c18a6a6
JayJHFE <dove92lee@gmail.com>
2024-09-02 21:58:51 +0900

    스케줄러 수정

--- PortFolio-vite/src/components/scheduler/leftTable/index.tsx
+++ PortFolio-vite/src/components/scheduler/leftTable/index.tsx
@@ -1,27 +1,49 @@
 function SchedulerLeftTable() {
-  const data = 5;
+  {
+    /*항상 실제 데이터보다 1크게작성해줘야함 */
+  }
+  const data = 6;
   return (
     <>
       {/* <div style={{ position: "sticky", top: 0, left: 0, zIndex: 100 }}> */}
       <div>
-        {Array.from({ length: data }, (_, i) => (
-          <div
-            key={i}
-            style={{
-              zIndex: 100,
-              width: "180px",
-              height: "30px",
-              backgroundColor: "blue",
-              display: "flex",
-              flexDirection: "column",
-              position: "sticky",
-              top: `${i * 30}px`,
-              left: 0, // 각 div가 서로 다른 위치에 놓이도록 조정
-            }}
-          >
-            1
-          </div>
-        ))}
+        {Array.from({ length: data }, (_, i) =>
+          i === 0 ? (
+            <div
+              key={i}
+              style={{
+                zIndex: 100,
+                width: "180px",
+                height: "30px",
+                backgroundColor: "blue",
+                display: "flex",
+                flexDirection: "column",
+                position: "sticky",
+                top: 0,
+                left: 0,
+              }}
+            >
+              할일목록
+            </div>
+          ) : (
+            <div
+              key={i}
+              style={{
+                zIndex: 100,
+                width: "180px",
+                height: "30px",
+                backgroundColor: "blue",
+                display: "flex",
+                flexDirection: "column",
+                position: "sticky",
+                top: `${i * 30}px`,
+                left: 0, // 각 div가 서로 다른 위치에 놓이도록 조정
+              }}
+            >
+              1
+            </div>
+          )
+        )}
       </div>
     </>
   );
--- PortFolio-vite/src/components/scheduler/table/index.tsx
+++ PortFolio-vite/src/components/scheduler/table/index.tsx
@@ -1,12 +1,14 @@
 function SchedulerTable() {
   const hoursArray = Array.from({ length: 25 }, (_, i) => i);
-  const data = 5;
+  const data = 6;
 
   return (
     <>
       <div style={{ position: "relative" }}>
         {/* div를 data의 길이만큼 생성 */}
+        {/* 첫번째 div만 다른 이름을 가진 div로 생성 */}
         {Array.from({ length: data }, (_, i) => (
+          // i == 0 ? () : ()
           <div
             key={i}
             style={{
@@ -17,6 +19,7 @@ function SchedulerTable() {
             }}
           >
             {hoursArray.map((index) => (
+              // i == 0 ? () : ()
               <div
                 key={index}
                 style={{
