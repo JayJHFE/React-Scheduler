c3cc3160db0604d713c8ea4576bd2d7dcb4f89b7
JayJHFE <dove92lee@gmail.com>
2024-09-03 21:06:51 +0900

    feat: 일정추가 input추가

--- PortFolio-vite/src/components/scheduler/right/rightUpperContainer.tsx
+++ PortFolio-vite/src/components/scheduler/right/rightUpperContainer.tsx
@@ -1,8 +1,21 @@
 function RightUpperContainer() {
   return (
-    <div className="right-upper-container">
-      <div className="right-upper-container__title">Scheduler</div>
-      <div className="right-upper-container__date">Today</div>
+    <div
+      className="right-upper-container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "300px",
+        backgroundColor: "blue",
+        alignItems: "center",
+      }}
+    >
+      <div className="right-upper-container__title">할일목록</div>
+      <input
+        className="right-upper-container__input"
+        style={{ width: "200px" }}
+      />
+      <button className="right-upper-container__button"></button>
     </div>
   );
 }
