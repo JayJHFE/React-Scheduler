8247232ad560970aeab7b4135584cff16b32d8e5
JayJHFE <dove92lee@gmail.com>
2024-09-04 21:14:56 +0900

    feat: redux에 state추가

---
+++ PortFolio-vite/src/redux/slice/tableLengthChangeReducer.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// 메인 페이지 렌더링을 위한 createSlice 생성
+export const tableLengthChangeReducer = createSlice({
+  name: "changeComponent",
+  initialState: {
+    //초기값 0
+    value: 0,
+  },
+  reducers: {
+    increase: (state) => {
+      state.value += 1; // 상태를 1 증가
+    },
+    decrease: (state) => {
+      state.value -= 1; // 상태를 1 감소
+    },
+  },
+});
+
+// componentChangeSlice의 actions를 export
+export const { increase, decrease } = tableLengthChangeReducer.actions;
+
+export default tableLengthChangeReducer.reducer;
