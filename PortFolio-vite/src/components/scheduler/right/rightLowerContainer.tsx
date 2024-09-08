d5d964cf467c26f62f668c7ccd0b137b97124f45
JayJHFE <dove92lee@gmail.com>
2024-09-08 15:11:37 +0900

    오른쪽하단 컴포넌트 생성

---
+++ PortFolio-vite/src/components/scheduler/right/rightLowerContainer.tsx
@@ -0,0 +1,40 @@
+function rightLowerContainer() {
+  return (
+    <div className="rightLowerContainer">
+      <div className="rightLowerContainer__header">
+        <h1>Upcoming</h1>
+      </div>
+      <div className="rightLowerContainer__content">
+        <div className="rightLowerContainer__content__item">
+          <div className="rightLowerContainer__content__item__header">
+            <h2>Meeting</h2>
+            <p>10:00 AM</p>
+          </div>
+          <div className="rightLowerContainer__content__item__content">
+            <p>Meeting with the team</p>
+          </div>
+        </div>
+        <div className="rightLowerContainer__content__item">
+          <div className="rightLowerContainer__content__item__header">
+            <h2>Meeting</h2>
+            <p>10:00 AM</p>
+          </div>
+          <div className="rightLowerContainer__content__item__content">
+            <p>Meeting with the team</p>
+          </div>
+        </div>
+        <div className="rightLowerContainer__content__item">
+          <div className="rightLowerContainer__content__item__header">
+            <h2>Meeting</h2>
+            <p>10:00 AM</p>
+          </div>
+          <div className="rightLowerContainer__content__item__content">
+            <p>Meeting with the team</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default rightLowerContainer;
